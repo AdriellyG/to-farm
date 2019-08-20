cdb179e9b832fd3d3cc37d3858536d5749f5c55b
AdriellyG <adriellygalvao@yahoo.com>
2019-08-20 19:10:41 -0300

    Adicionado 'scope :api'

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,22 +1,23 @@
 Rails.application.routes.draw do
-  resources :plantios
-  resources :tipo_movimentacaos
-  resources :status_plantios
-  resources :areas
-  resources :tipo_solos
-  resources :funcionarios
-  resources :cargos
-  resources :local_fisicos
-  resources :acesso_usuario_fazendas
-  resources :cultivos
-  resources :usuarios
-  resources :fazenda_has_planos
-  resources :fazendas
-  resources :pessoa_fisicas
-  resources :pessoa_juridicas
-  resources :pessoas
-  resources :pes_pessoas
-  resources :planos
-  resources :tipo_cultivos
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope :api, defaults: {format: :json} do
+    resources :plantios
+    resources :tipo_movimentacaos
+    resources :status_plantios
+    resources :areas
+    resources :tipo_solos
+    resources :funcionarios
+    resources :cargos
+    resources :local_fisicos
+    resources :acesso_usuario_fazendas
+    resources :cultivos
+    resources :usuarios
+    resources :fazenda_has_planos
+    resources :fazendas
+    resources :pessoa_fisicas
+    resources :pessoa_juridicas
+    resources :pessoas
+    resources :pes_pessoas
+    resources :planos
+    resources :tipo_cultivos
+  end
 end
