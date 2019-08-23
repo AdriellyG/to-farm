5e6cf33eac1df7b430817ac7f12195c06a5cd026
AdriellyG <adriellygalvao@yahoo.com>
2019-08-23 16:55:26 -0300

    Correção tabela fazenda_has_planos

--- back/db/migrate/20190819155228_create_fazenda_has_planos.rb
+++ back/db/migrate/20190819155228_create_fazenda_has_planos.rb
@@ -3,6 +3,7 @@ class CreateFazendaHasPlanos < ActiveRecord::Migration[5.2]
     create_table :fazenda_has_planos do |t|
       t.date :dt_inicio
       t.references :fazendas, foreign_key: true
+      t.references :planos,   foreign_key: true
       t.boolean :ativo
 
       t.timestamps
