6a09c3a0616d499e7658c0506bd7731910a95553
AdriellyG <adriellygalvao@yahoo.com>
2019-08-28 17:34:42 -0300

    Seed para cultivo

---
+++ back/db/seeds/cultivo.rb
@@ -0,0 +1,21 @@
+puts "-> Creating seeds for Cultivo..."
+
+Cultivo.create!({
+    nome: 'Morango',
+    tipo_cultivo_id: 1,
+    fazenda_id: 1
+})
+
+Cultivo.create!({
+    nome: 'Alface',
+    tipo_cultivo_id: 2,
+    fazenda_id: 1
+})
+
+Cultivo.create!({
+    nome: 'Jilo',
+    tipo_cultivo_id: 3,
+    fazenda_id: 1
+})
+
+puts "-> Created!"
