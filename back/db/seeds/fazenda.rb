e6419f0ee58f1ea28ddd4743fb4d3693f03caaf4
AdriellyG <adriellygalvao@yahoo.com>
2019-08-28 17:34:14 -0300

    Seed para fazenda

---
+++ back/db/seeds/fazenda.rb
@@ -0,0 +1,33 @@
+puts "-> Creating seeds for Fazenda..."
+
+Fazenda.create!({
+    nome: 'Fazenda 1',
+    rua: 'Rua 1',
+    numero: '1',
+    cep: 12570000,
+    estado: 'ES',
+    cidade: 'Cidade 1',
+    pessoa_id: 1
+})
+
+Fazenda.create!({
+    nome: 'Fazenda 2',
+    rua: 'Rua 2', 
+    numero: '2',
+    cep: 12570001,
+    estado: 'RJ',
+    cidade: 'Cidade 2',
+    pessoa_id: 2
+})
+
+Fazenda.create!({
+    nome: 'Fazenda 3',
+    rua: 'Rua 3', 
+    numero: '3',
+    cep: 12570003,
+    estado: 'SP',
+    cidade: 'Cidade 3',
+    pessoa_id: 3
+})
+
+puts "-> Created!"
