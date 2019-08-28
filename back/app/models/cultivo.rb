9e12a3fa10db7417b5cac27ff2477cbc432e1d14
AdriellyG <adriellygalvao@yahoo.com>
2019-08-28 18:28:05 -0300

    Ajuste model cultivo

--- back/app/models/cultivo.rb
+++ back/app/models/cultivo.rb
@@ -1,4 +1,4 @@
 class Cultivo < ApplicationRecord
-  belongs_to :tipo_cultivos
-  belongs_to :fazendas
+  belongs_to :tipo_cultivos, class_name: "TipoCultivo", optional: true
+  belongs_to :fazendas, class_name: "Fazenda", optional: true
 end
