ee33c6f625ee927b11eccce2b3c2c5e052c2a1dc
AdriellyG <adriellygalvao@yahoo.com>
2019-08-30 17:16:42 -0300

    Padronização de escrita

--- back/db/seeds/plano.rb
+++ back/db/seeds/plano.rb
@@ -1,6 +1,6 @@
 puts "-> Creating seeds for Plano..."
 
-Plano.create!(nome: "Básico", valor: 70)
+Plano.create!(nome: "Básico",  valor: 70)
 Plano.create!(nome: "Premium", valor: 120)
 
 puts "-> Created!"
--- back/db/seeds/usuario.rb
+++ back/db/seeds/usuario.rb
@@ -1,8 +1,8 @@
 puts "-> Creating seeds for Usuario..."
 
 Usuario.create!(email: "adriellygalvao@yahoo.com", senha: "mortadela124", pessoa_fisica_id: 1)
-Usuario.create!(email: "adrielly@yahoo.com", senha: "mortadela443", pessoa_fisica_id: 2)
-Usuario.create!(email: "adriellyg@yahoo.com", senha: "mortadela041", pessoa_fisica_id: 3)
+Usuario.create!(email: "adrielly@yahoo.com",       senha: "mortadela443", pessoa_fisica_id: 2)
+Usuario.create!(email: "adriellyg@yahoo.com",      senha: "mortadela041", pessoa_fisica_id: 3)
 
 puts "-> Created!"
 puts "========================================"
