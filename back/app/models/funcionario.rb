e357f3577d01eaf731c6a91828367e12ecfc8d5a
AdriellyG <adriellygalvao@yahoo.com>
2019-09-04 14:41:46 -0300

    Correção relações entre models

--- back/app/models/funcionario.rb
+++ back/app/models/funcionario.rb
@@ -1,4 +1,4 @@
 class Funcionario < ApplicationRecord
-  belongs_to :cargos
-  belongs_to :usuarios
+  belongs_to :cargos, class_name: "Cargo", optional: true
+  belongs_to :usuarios, class_name: "Usuario", optional: true
 end
--- back/app/models/usuario.rb
+++ back/app/models/usuario.rb
@@ -1,3 +1,4 @@
 class Usuario < ApplicationRecord
   belongs_to :pessoa_fisica
+  has_one :funcionario
 end
