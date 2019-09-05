f4e6d075b93f90a3b6dec721f8a85c7d4b7b80af
AdriellyG <adriellygalvao@yahoo.com>
2019-09-05 17:48:08 -0300

    Seeds for Area

--- back/app/models/area.rb
+++ back/app/models/area.rb
@@ -1,5 +1,5 @@
 class Area < ApplicationRecord
-  belongs_to :fazendas
-  belongs_to :tipo_solos
-  belongs_to :local_fisicos
+  belongs_to :fazendas,      class_name: "Fazenda", optional: true
+  belongs_to :tipo_solos,    class_name: "TipoSolo", optional: true
+  belongs_to :local_fisicos, class_name: "LocalFisico", optional: true
 end
