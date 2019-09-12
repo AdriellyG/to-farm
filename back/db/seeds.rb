1151f52967a4919dfe9a2ae3142165a2d8996efb
AdriellyG <adriellygalvao@yahoo.com>
2019-09-11 21:09:10 -0300

    Seed fazenda atualizado

--- back/db/seeds.rb
+++ back/db/seeds.rb
@@ -5,24 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-puts "****************************************"
-puts 'Starting seeds...'
-
-puts "========================================"
-load File.join(Rails.root, 'db', 'seeds', 'pessoa.rb')
-load File.join(Rails.root, 'db', 'seeds', 'pessoa_fisica.rb')
-load File.join(Rails.root, 'db', 'seeds', 'pessoa_juridica.rb')
-load File.join(Rails.root, 'db', 'seeds', 'fazenda.rb')
-load File.join(Rails.root, 'db', 'seeds', 'plano.rb')
-load File.join(Rails.root, 'db', 'seeds', 'fazenda_has_plano.rb')
-load File.join(Rails.root, 'db', 'seeds', 'usuario.rb')
-load File.join(Rails.root, 'db', 'seeds', 'tipo_cultivo.rb')
-load File.join(Rails.root, 'db', 'seeds', 'cultivo.rb')
-load File.join(Rails.root, 'db', 'seeds', 'tipo_solo.rb')
-load File.join(Rails.root, 'db', 'seeds', 'local_fisico.rb')
-load File.join(Rails.root, 'db', 'seeds', 'area.rb')
-load File.join(Rails.root, 'db', 'seeds', 'cargo.rb')
-load File.join(Rails.root, 'db', 'seeds', 'funcionario.rb')
-
-puts 'Finished!'
-puts "****************************************"
