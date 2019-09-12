a928b9a4f497e024b69ecbeb8f9d3bd8d589b0fd
AdriellyG <adriellygalvao@yahoo.com>
2019-09-11 21:07:50 -0300

    Migrates atualizados

--- back/db/migrate/20190819154827_create_pessoa_juridicas.rb
+++ back/db/migrate/20190819154827_create_pessoa_juridicas.rb
@@ -2,7 +2,7 @@ class CreatePessoaJuridicas < ActiveRecord::Migration[5.2]
   def change
     create_table :pessoa_juridicas do |t|
       t.string :cnpj
-      t.references :pessoa, foreign_key: true
+      t.references :pessoas, foreign_key: true
 
       t.timestamps
     end
