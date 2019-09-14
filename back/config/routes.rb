2488524e1e87c441c9c2c9ce4d9e94443f84c093
AdriellyG <adriellygalvao@yahoo.com>
2019-09-14 06:10:23 -0300

    Rotas e documentação atualizadas

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,21 +1,22 @@
 Rails.application.routes.draw do
-  resources :cultivos
-  resources :usuarios
-  resources :fazenda_has_planos
-  resources :fazendas
-  resources :pessoa_fisicas
-  resources :pessoa_juridicas
-  resources :pessoas
-  resources :pes_pessoas
-  resources :planos
-  resources :tipo_cultivos
-  resources :plantios
-  resources :tipo_movimentacaos
-  resources :status_plantios
-  resources :areas
-  resources :tipo_solos
-  resources :funcionarios
-  resources :cargos
-  resources :local_fisicos
-  resources :acesso_usuario_fazendas
+  scope :api, defaults: {format: :json} do
+    resources :cultivos
+    resources :usuarios
+    resources :fazenda_has_planos
+    resources :fazendas
+    resources :pessoa_fisicas
+    resources :pessoa_juridicas
+    resources :pessoas
+    resources :planos
+    resources :tipo_cultivos
+    resources :plantios
+    resources :tipo_movimentacaos
+    resources :status_plantios
+    resources :areas
+    resources :tipo_solos
+    resources :funcionarios
+    resources :cargos
+    resources :local_fisicos
+    resources :acesso_usuario_fazendas
+  end
 end
